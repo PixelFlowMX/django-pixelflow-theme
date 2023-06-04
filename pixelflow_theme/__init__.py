3d7d985ad73802af6584b4f0d83f9ba2d6267d1d
Dharwin Perez <dharwin@getfairplay.com>
2023-06-03 23:26:01 -0600

    Add metadata

---
+++ pixelflow_theme/__init__.py
@@ -0,0 +1,7 @@
+from pixelflow_theme.metadata import __author__, __title__, __version__
+
+__all__ = [
+    "__author__",
+    "__title__",
+    "__version__",
+]
---
+++ pixelflow_theme/metadata.py
@@ -0,0 +1,4 @@
+__author__ = "Dharwin Perez"
+__email__ = "dharwin@pixelflow.com.mx"
+__title__ = "django-pixelflow-theme"
+__version__ = "0.0.1"
